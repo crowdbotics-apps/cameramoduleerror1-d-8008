fc5a7852816606074b66b6c4dd5dc295f93341c6
Crowdbotics <team@crowdbotics.com>
2020-07-23 09:21:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cameramoduleerror1_d_8008/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cameramoduleerror1_d_8008;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "cameramoduleerror1_d_8008";
+    }
+}
